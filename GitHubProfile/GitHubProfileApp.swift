85661a678ad0e8c2c5830e215144810244a3efe1
ricardo <ricardo@>
2023-11-26 18:55:46 -0300

    Initial Commit

---
+++ GitHubProfile/GitHubProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProfileApp.swift
+//  GitHubProfile
+//
+//  Created by Ricardo on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
