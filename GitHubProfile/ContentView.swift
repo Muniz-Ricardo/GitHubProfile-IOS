1214db81162045a6f48cfa40d4e397a8d2d8e8c3
ricardo <ricardo.muniz.devv@gmail.com>
2023-11-27 03:03:14 -0300

    design interface

--- GitHubProfile/ContentView.swift
+++ GitHubProfile/ContentView.swift
@@ -8,14 +8,98 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputNameGH: String = ""
+    @State private var user: GitUser?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            AsyncImage(url: URL(string: user?.avatarUrl ?? "")) {
+                image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(width: 100, height: 100)
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.accentColor)
+                    .frame(width: 100, height: 100)
+            }
+            
+            Text(user?.login ?? "username-placeholder")
+                .font(.custom("Roboto-Medium", size: 20))
+                .padding(.top, 10)
+                .foregroundColor(.white)
+            
+            Text(user?.bio ?? "Here is the bio of the user searched on GitHub.")
+                .font(.custom("Roboto-Light", size: 18))
+                .foregroundColor(.white)
+                .padding(.horizontal, 20)
+                .padding(.top, 2)
+                .multilineTextAlignment(.center)
+            
+            TextField("Digite aqui o nome do perfil no GitHub…", text: $inputNameGH)
+                .textFieldStyle(.roundedBorder)
+                .foregroundColor(.black)
+                .colorInvert()
+                .multilineTextAlignment(.center)
+                .padding(.top, 20)
+            
+            Button(action: {
+                Task {
+                    do {
+                        user = try await getUserGitHub(name: inputNameGH)
+                    } catch GHError.invalidURL {
+                        print("invalid url…")
+                    } catch GHError.invalidResponse {
+                        print("invalid response…")
+                    } catch GHError.invalidData {
+                        print("invalid data…")
+                    } catch {
+                        print("unexpected error")
+                    }
+                }
+            }) {
+                Text("Buscar")
+                    .padding(10)
+                    .padding(.horizontal, 10)
+            }
+            .background(Color.white)
+            .foregroundColor(.black)
+            .cornerRadius(6)
+            .padding(26)
+            .frame(maxWidth: .infinity)
+            
+            
+            Spacer()
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .ignoresSafeArea()
+        
+    }
+    
+    func getUserGitHub(name: String) async throws -> GitUser? {
+        let endpoint = "https://api.github.com/users/\(name)"
+        
+        guard let url = URL(string: endpoint) else { throw GHError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw GHError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(GitUser.self, from: data)
+        } catch {
+            throw GHError.invalidData
+        }
+        
     }
 }
 
---
+++ GitHubProfile/Data/GHError.swift
@@ -0,0 +1,14 @@
+//
+//  GHError.swift
+//  GitHubProfile
+//
+//  Created by Ricardo on 26/11/23.
+//
+
+import Foundation
+
+enum GHError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
